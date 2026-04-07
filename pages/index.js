c36da5d0d13e2e10f5b1dd53c5331a00e20fc87d
Posselinha <gabrielfilipeposs@hotmail.com>
2026-04-07 15:14:34 -0300

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Nico, sou muito grato por te conhecer. Se você ver essa mensagem, mande seu Ryuji Sakamoto FOR REAL!?</h1>
+    return <h1>Nico, sou muito grato por te conhecer. Se você ver essa mensagem, mande seu Ryuji Sakamoto FOR REAL!!?</h1>
 }
 
 export default Home;
