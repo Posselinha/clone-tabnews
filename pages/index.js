bc8f359b121f2f19713164a1b1d298950110ba5f
Posselinha <gabrielfilipeposs@hotmail.com>
2026-04-15 11:39:34 -0300

    testando metadados

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,36 @@ function Home() {
   return (
     <>
       <Head>
-        <title>Teste de titulo</title>
+        <meta charset="UTF-8"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <link rel="stylesheet" href="./css/main.css"></link>
+        <title>Document</title>
+        <meta
+          name="description"
+          content="Portfolio de Gabriel Possela. Entre no meu portfolio e venha conhecer meus projetos."
+        ></meta>
+        <meta
+          name="keywords"
+          content="web development, programming, portfolio, Gabriel Possela, website services"
+        ></meta>
+        <meta name="author" content="Gabriel Possela"></meta>
+
+        <meta property="og:title" content="Gabriel Possela | Portfolio"></meta>
+        <meta
+          property="og:description"
+          content="Venha conhecer o Portfolio de Gabriel Possela. Programador FullStack voltado para desenvolvimento web."
+        ></meta>
+        <meta property="og:url" content="https://gabrielpossela.com.br"></meta>
+        <meta property="og:type" content="website"></meta>
+
+        <link rel="canonical" href="https://gabrielpossela.com.br"></link>
+        <link rel="stylesheet" href="./src/assets/css/main.css"></link>
       </Head>
 
-      <h1>
-        Testando metadados
-      </h1>
+      <h1>Testando metadados</h1>
     </>
   );
 }
